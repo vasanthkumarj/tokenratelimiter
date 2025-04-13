68dee8f7819af14577d8cf3872c2764d43002c14
Vasanth kumar <vasanthkumar.j@outlook.com>
2025-04-13 19:49:44 +0530

    Token rate limiter

---
+++ src/main/java/com/learningseries/concurrency/tokenratelimiter/TokenratelimiterApplication.java
@@ -0,0 +1,13 @@
+package com.learningseries.concurrency.tokenratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenratelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenratelimiterApplication.class, args);
+	}
+
+}
